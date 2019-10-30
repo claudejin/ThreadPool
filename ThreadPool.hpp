7b1a26b098c53a428603478cc836c5d2f7de5a73
Mingyu Jin <mgjin@vieworks.com>
2019-10-30 18:00:26 +0900

    Header-only version

--- ThreadPool.hpp
+++ ThreadPool.hpp
@@ -1,6 +1,80 @@
-#include "ThreadPool.h"
+#pragma once
 
-ThreadPool::ThreadPool(size_t poolSize)
+#include <vector>				// std::vector
+#include <queue>				// std::queue
+#include <memory>				// std::make_shared, std::shared_ptr
+#include <thread>				// std::thread
+#include <mutex>				// std::mutex, std::unique_lock
+#include <condition_variable>	// std::condition_variable
+#include <future>				// std::future, std::packaged_task
+#include <functional>			// std::bind, std::function
+#include <stdexcept>			// std::runtime_error
+#include <algorithm>			// std::max
+
+class ThreadPool {
+public:
+	ThreadPool(size_t poolSize = (std::max)(2u, std::thread::hardware_concurrency()));
+	~ThreadPool();
+
+	template<class F, class... Args>
+	auto enqueue(F&& f, Args&&... args)
+		->std::future<typename std::result_of<F(Args...)>::type>;
+	void clearTask();
+	size_t getWaitingTaskSize();
+	void waitUntilQueueEmpty();
+
+	size_t getPoolSize();
+	void setPoolSize(size_t limit);
+	size_t getNumAvailable();
+	void waitUntilAllIdle();
+
+private:
+	void emplace_back_worker(std::size_t worker_number);
+
+	std::size_t pool_size;
+	std::atomic<std::size_t> running;
+
+	std::vector< std::thread > workers;
+	std::queue< std::function<void()> > tasks;
+
+	// synchronization
+	std::condition_variable condition_consumers;
+	std::condition_variable condition_task_update;
+	std::condition_variable condition_worker_update;
+
+	std::mutex mutex_task;
+	std::mutex mutex_worker;
+	bool stop;
+	bool cleared;
+};
+
+// add new work item to the pool
+template<class F, class... Args>
+auto ThreadPool::enqueue(F&& f, Args&&... args)
+-> std::future<typename std::result_of<F(Args...)>::type>
+{
+	using return_type = typename std::result_of<F(Args...)>::type;
+
+	auto task = std::make_shared< std::packaged_task<return_type()> >(
+		std::bind(std::forward<F>(f), std::forward<Args>(args)...)
+		);
+
+	std::future<return_type> res = task->get_future();
+	{
+		std::unique_lock<std::mutex> lock(mutex_task);
+
+		// don't allow enqueueing after stopping the pool
+		if (stop)
+			throw std::runtime_error("enqueue on stopped ThreadPool");
+
+		tasks.emplace([this, task](){ if (!this->cleared) (*task)(); });
+	}
+
+	condition_consumers.notify_one();	
+	return res;
+}
+
+inline ThreadPool::ThreadPool(size_t poolSize)
 	: pool_size(poolSize)
 	, running(0)
 	, stop(false)
@@ -12,7 +86,7 @@ ThreadPool::ThreadPool(size_t poolSize)
 		emplace_back_worker(i);
 }
 
-ThreadPool::~ThreadPool()
+inline ThreadPool::~ThreadPool()
 {
 	std::unique_lock<std::mutex> lock(mutex_task);
 	stop = true;
@@ -24,7 +98,7 @@ ThreadPool::~ThreadPool()
 	condition_consumers.wait(lock, [this]{ return this->workers.empty(); });
 }
 
-void ThreadPool::clearTask()
+inline void ThreadPool::clearTask()
 {
 	std::unique_lock<std::mutex> lock(this->mutex_task);
 
@@ -36,19 +110,25 @@ void ThreadPool::clearTask()
 	condition_task_update.notify_all();
 }
 
-void ThreadPool::waitUntilQueueEmpty()
+inline size_t ThreadPool::getWaitingTaskSize()
+{
+	std::unique_lock<std::mutex> lock(this->mutex_task);
+	return tasks.size();
+}
+
+inline void ThreadPool::waitUntilQueueEmpty()
 {
 	std::unique_lock<std::mutex> lock(this->mutex_task);
 	this->condition_task_update.wait(lock,
 		[this]{ return this->tasks.empty(); });
 }
 
-size_t ThreadPool::getSize()
+inline size_t ThreadPool::getPoolSize()
 {
 	return pool_size;
 }
 
-void ThreadPool::setSize(std::size_t limit)
+inline void ThreadPool::setPoolSize(std::size_t limit)
 {
 	if (limit < 1)
 		limit = 1;
@@ -72,19 +152,19 @@ void ThreadPool::setSize(std::size_t limit)
 		this->condition_consumers.notify_all();
 }
 
-size_t ThreadPool::getNumAvailable()
+inline size_t ThreadPool::getNumAvailable()
 {
 	return pool_size - running;
 }
 
-void ThreadPool::waitUntilAllIdle()
+inline void ThreadPool::waitUntilAllIdle()
 {
 	std::unique_lock<std::mutex> lock(this->mutex_worker);
 	this->condition_worker_update.wait(lock,
-		[this]{ return this->getNumAvailable() == this->getSize(); });
+		[this]{ return (this->running == 0); });
 }
 
-void ThreadPool::emplace_back_worker(std::size_t worker_number)
+inline void ThreadPool::emplace_back_worker(std::size_t worker_number)
 {
 	std::unique_lock<std::mutex> lock(this->mutex_worker);
 
@@ -128,14 +208,16 @@ void ThreadPool::emplace_back_worker(std::size_t worker_number)
 					this->tasks.pop();
 					if (this->tasks.empty())
 						condition_task_update.notify_all();
+
+					std::atomic_fetch_add_explicit(&this->running,
+						std::size_t(1),
+						std::memory_order_relaxed);
+
+					condition_worker_update.notify_all();
 				}
 				else
 					continue;
 			}
-
-			std::atomic_fetch_add_explicit(&this->running,
-				std::size_t(1),
-				std::memory_order_relaxed);
 
 			task();
 
